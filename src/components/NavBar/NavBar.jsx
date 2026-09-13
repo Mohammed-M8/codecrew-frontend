63ca1ff3e1bb4913ae9cb2308a59659eea07ae0d
Mohammed-M8 <202200692@student.polytechnic.bh>
2026-09-13 13:24:16 +0300

    added bootstrap package+ refactored navbar for test

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -12,25 +12,53 @@ const NavBar = () => {
   }
 
   return (
-    <nav>
-      <ul>
+<nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">Code Crew</Link>
 
-        { user
-          ?
+    <button
+      className="navbar-toggler"
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav ms-auto align-items-lg-center">
+        {user ? (
           <>
-            <li>Hello {user.username}</li>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/" onClick={handleSignOut}>Sign Out</Link></li>
+            <li className="nav-item">
+              <span className="nav-link disabled">Hello, {user.username}</span>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/" onClick={handleSignOut}>Sign Out</Link>
+            </li>
           </>
-          :
+        ) : (
           <>
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to='/sign-up'>Sign Up</Link></li>
-            <li><Link to='/sign-in'>Sign In</Link></li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Dashboard</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sign-up">Sign Up</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/sign-in">Sign In</Link>
+            </li>
           </>
-        }
+        )}
       </ul>
-    </nav>
+    </div>
+  </div>
+</nav>
   );
 };
 
