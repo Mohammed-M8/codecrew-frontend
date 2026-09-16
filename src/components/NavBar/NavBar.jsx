694e673731e67fe649da6e0e7414557ae84f875c
Mohammed-M8 <202200692@student.polytechnic.bh>
2026-09-16 13:02:38 +0300

    import css fix

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { Link } from 'react-router';
 import { UserContext } from '../../contexts/UserContext';
-import './NavBar.css'
+import './Navbar.css'
 
 const NavBar = () => {
 
